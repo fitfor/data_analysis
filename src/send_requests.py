f4db3d257deddd4e434773b0331d8dd86f8d08e1
Cingis Kratochvil <cingis.kratochvil@steadforce.com>
2022-03-25 09:06:20 +0100

    Add code snipet for the Cloud deployment

--- src/send_requests.py
+++ src/send_requests.py
@@ -1,10 +1,11 @@
 import requests
 
 
-#url = 'http://0.0.0.0:5050/get-recomm/'
-url = 'https://recomm-microservice.herokuapp.com/'
+#url = 'http://127.0.0.1:5000/get-recomm/'
+url = 'https://recomm-microservice.herokuapp.com/get-recomm/'
 
 params = {'act1': 'Focus excercise', 'act2': 'Sleeping excercise', 'act3': 'Flexibility excercise'}
+#params = {'act1': 'HealthyEating', 'act2': 'Endurance excercise', 'act3': 'Motivation excercise'}
 
 r = requests.post(url, params=params)
 
